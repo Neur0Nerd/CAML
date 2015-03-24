622bba4708a94fee71731a2c5fe348127d5615f5
Neur0Nerd <neur0nerd@hotmail.com>
2015-03-24 09:01:27 +0100

    TP2  2 Morse
    FINAL

--- TP2 Morse/mebare_h/src/exo4.ml
+++ TP2 Morse/mebare_h/src/exo4.ml
@@ -9,12 +9,51 @@ word_to_morse(
     iter ((String.length str) - 1) [])
 ;;
 
-let rec char_list_to_string_list str  = match str with
-  |e::[] -> String.make 1 e 
-  |e::l -> (String.make 1 e ) ^ (char_list_to_string_list l );;
-
 let final str = 
 char_list_to_string_list str
   let rec balo str = match str with
     |[] -> []
     |e::l -> e^(balo l) ;;*)
+*)
+
+let letter_to_morse = function
+  |'a' | 'A' -> ['.' ; '-']
+  |'b' | 'B' -> ['-' ; '.' ; '.' ; '.']
+  |'c' | 'C' -> ['-' ; '.' ; '-' ; '.']
+  |'d' | 'D' -> ['-' ; '.' ; '.']
+  |'e' | 'E' -> ['.']
+  |'f' | 'F' -> ['.' ; '.' ; '-' ; '.'] 
+  |'g' | 'G' -> ['-' ; '-' ; '.']
+  |'h' | 'H' -> ['.' ; '.' ; '.' ; '.']
+  |'i' | 'I' -> ['.'; '.']
+  |'j' | 'J' -> ['.' ; '-' ; '-' ; '-']
+  |'k' | 'K' -> ['-' ; '.' ; '-']
+  |'l' | 'L' -> ['.' ; '-' ; '.' ; '.']
+  |'m' | 'M' -> ['-' ; '-']
+  |'n' | 'N' -> ['-' ; '.']
+  |'o' | 'O' -> ['-' ; '-' ; '-']
+  |'p' | 'P' -> ['.' ; '-' ; '-' ; '.']
+  |'q' | 'Q' -> ['-' ; '-' ; '.' ; '-']
+  |'r' | 'R' -> ['.' ; '-' ; '.']
+  |'s' | 'S' -> ['.' ; '.' ; '.']
+  |'t' | 'T' -> ['-']
+  |'u' | 'U' -> ['.' ; '.' ; '-']
+  |'v' | 'V' -> ['.' ; '.' ; '.' ; '-']
+  |'w' | 'W' -> ['.' ; '-' ; '-']
+  |'x' | 'X' -> ['-' ; '.' ; '.' ; '-']
+  |'y' | 'Y' -> ['-' ; '.' ; '-' ; '-']
+  |'z' | 'Z' -> ['-' ; '-' ; '.' ; '.']
+  |' ' -> ['/']		  
+  | _  -> failwith "The type of your input is not a char, please try with another data";;  
+  
+let latin_to_morse str =
+  let rec char_list_to_string_list str  = match str with
+    |[] -> ""
+    |e::l -> (String.make 1 e ) ^ (char_list_to_string_list l )
+ in
+let rec iter str i =
+  if i < (String.length str) then (char_list_to_string_list(letter_to_morse(str.[i])))^(" " ^iter str (i+1))
+  else ""
+in
+  iter str 0;;
+(*latin_to_morse "Vive Les Listes" ;;*)
